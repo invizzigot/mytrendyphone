2f98635305487edbea040038a314445488776dce
invizzigot <linuxmaggic@gmail.com>
2025-01-30 18:06:36 +0100

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Fabric from "../components/Fabric.vue";
+import NewPage from "../components/NewPage.vue";
 
 // import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <Fabric />
+    <NewPage />
     <!-- <Tabs /> -->
   </main>
 </template>
