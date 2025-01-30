ff3e493d4a2ce368f8f8081fd6a6210388a99855
invizzigot <linuxmaggic@gmail.com>
2025-01-30 20:19:39 +0100

    Update NewPage

--- src/components/NewPage.vue
+++ src/components/NewPage.vue
@@ -5481,7 +5481,7 @@ export default {
     },
 
     render(ctx, left, top, styleOverride, fabricObject) {
-      fabric.Image.fromURL("images/add-square.svg",  (img) => {
+      fabric.Image.fromURL("https://mytrendyphone.onrender.com/images/add-square.svg",  (img) => {
         img.scaleToWidth(48);
         img.scaleToHeight(48);
         ctx.drawImage(img._element, left - 16, top - 16, 48, 48);
