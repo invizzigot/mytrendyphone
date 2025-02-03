6c1e58e05ae18c91a5768300f938ac72c15578f2
invizzigot <linuxmaggic@gmail.com>
2025-02-03 13:42:36 +0100

    Update NewPage.vue

--- src/components/NewPage.vue
+++ src/components/NewPage.vue
@@ -1,4 +1,4 @@
-<template class="flex-row flex overflow-hidden">
+<template class="flex-row flex h-dvh overflow-hidden">
   <div class=" flex-row flex items-center justify-start relative w-[100vw] p-1  h-[70px] shadow-inner bg-gray-200"
   >
     <div
