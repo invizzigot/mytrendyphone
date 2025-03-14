0157d108d6b15802de33ef323bef430fa1769de0
invizzigot <linuxmaggic@gmail.com>
2025-03-14 07:27:09 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    // ...
+    extend: {
+      backgroundColor: {
+        'main-color-bg': '#00aaff', // Add your custom color here
+      }
+    }
   }
   // ...
 }
